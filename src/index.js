3fc6ec55bc3ff4715bd8240ea0e3fe2004be30a8
rajanmoliya <rajanmoliya2000@gmail.com>
2024-05-31 22:53:52 +0530

    connect to Mongo Atlas

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import "dotenv/config";
+import connectDB from "./db/index.js";
+
+connectDB();
+
+// 2nd approach iffy function execute right 1st fn after ()()
+
+// (async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+
+//     app.on("error", (error) => {
+//       console.log("Error:", error);
+//       throw error;
+//     });
+//     app.listen(process.env.PORT, () => {
+//       console.log(`App is listening on port ${process.env.PORT}`);
+//     });
+//   } catch (error) {
+//     console.error("ERROR: ", error);
+//     throw error;
+//   }
+// })();
