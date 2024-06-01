eb2825b14c801c4c78b0fc27df694188e69cacb4
rajanmoliya <rajanmoliya2000@gmail.com>
2024-06-01 16:07:27 +0530

    init/add models

--- src/index.js
+++ src/index.js
@@ -1,23 +1,13 @@
 import "dotenv/config";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
-connectDB();
-
-// 2nd approach iffy function execute right 1st fn after ()()
-
-// (async () => {
-//   try {
-//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-
-//     app.on("error", (error) => {
-//       console.log("Error:", error);
-//       throw error;
-//     });
-//     app.listen(process.env.PORT, () => {
-//       console.log(`App is listening on port ${process.env.PORT}`);
-//     });
-//   } catch (error) {
-//     console.error("ERROR: ", error);
-//     throw error;
-//   }
-// })();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Database connection failed: ", err);
+  });
